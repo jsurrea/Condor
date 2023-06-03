4bbbeb1f170acf5d2b21fb61fef1a0a7a827fa8e
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-06-03 13:08:18 -0500

    Segmentation

---
+++ Condor/Video/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['detect_objects_in_video']
--- Condor/Video/segmentation.py
+++ Condor/Video/segmentation.py
@@ -1,11 +1,17 @@
 import os
 import av
+import cv2
 import torch
+import easyocr
+import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+
 
 def detect_objects_in_video(video_path, output_path):
     """
+    Detects objects in the given video and saves the results to the given output path
     """
 
     # Create the model
@@ -13,6 +19,7 @@ def detect_objects_in_video(video_path, output_path):
     sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
     sam.to(device=device)
     mask_generator = SamAutomaticMaskGenerator(sam)
+    reader = easyocr.Reader(["es"], gpu=torch.cuda.is_available())
 
     # Open the video file using PyAV
     container = av.open(video_path)
@@ -22,6 +29,10 @@ def detect_objects_in_video(video_path, output_path):
     img_dir = os.path.join(output_dir, "IMG")
     os.makedirs(name=img_dir, exist_ok=True)
 
+    # Create the csv file
+    with open(output_path, "w") as f:
+        f.write("id,object_type,time,coordinates_text\n")
+
     # Iterate over each frame in the video
     for i, frame in enumerate(container.decode(video=0)):
         time = frame.time
@@ -30,15 +41,28 @@ def detect_objects_in_video(video_path, output_path):
         # Discard frames with a low variance of pixel values
         # or with temporal proximity to the previous frame
         if i % 100 == 0 and frame.var() > 3000:
-            masks = segment_frame(frame, mask_generator)
-            masks.save(os.path.join(img_dir, '{i:%08d}.jpg'))
+            segment_frame(frame, mask_generator, os.path.join(img_dir, f'{i:08d}.png'))
+            seconds = int(int(time) % 60)
+            minutes = int((time // 60) % 60)
+            hours = int(time // 3600)
+            coordinates = get_coordinates(reader, frame)
+            time = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+            
+            # Append to the csv file
+            with open(output_path, "a") as f:
+                f.write(f"{i},object,{time},\"{coordinates}\"\n")
 
     # Close the video file
     container.close()
 
-def segment_frame(frame, mask_generator):
+    # Free memory
+    del sam
+    del mask_generator
+    del reader
+
+def segment_frame(frame, mask_generator, savepath, top_n=15):
     """
-    
+    Performs inference on the given frame and returns the segmentation masks
     """
 
     # Generate the masks from SAM
@@ -48,19 +72,22 @@ def segment_frame(frame, mask_generator):
     confidences = list(map(lambda x:x["predicted_iou"], masks))
     masks = list(map(lambda x:x[0], sorted(zip(masks, confidences), key=lambda x:x[1], reverse=True)))
 
-    # Create PIL from segmentation masks
-    masks = show_anns(masks)
-    masks = Image.fromarray(masks)
+    # Save results
+    show_anns(frame, masks[:top_n], savepath)
 
-    return masks
+def show_anns(frame, anns, savepath):
+    """
+    Creates an image with the given annotations and saves it to the given path
+    """
 
-def show_anns(anns):
-    """
-    Creates an image with the segmentation masks
-    """
+    plt.figure(figsize=(20,20))
+    plt.imshow(frame)
+    
     if len(anns) == 0:
         return
     sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
 
     img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
     img[:,:,3] = 0
@@ -68,7 +95,15 @@ def show_anns(anns):
         m = ann['segmentation']
         color_mask = np.concatenate([np.random.random(3), [0.35]])
         img[m] = color_mask
-    
-    return img
+        
+    ax.imshow(img)
+    plt.axis('off')
+    plt.savefig(savepath, bbox_inches='tight') 
 
+def get_coordinates(reader, frame):
+    """
+    Returns the coordinates of the given frame
+    """
+    result = reader.readtext(frame, paragraph=False)
+    text = " ".join(map(str, result))
 
